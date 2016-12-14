00f956eef63ad85009651967b597cd9046271a2a
Jonathan Chrisp <jonathan.chrisp@gmail.com>
2016-12-14 19:24:54 +0000

    NOTICKET added index.js to run express server with configured routes

---
+++ index.js
@@ -0,0 +1,19 @@
+const bodyParser = require('body-parser');
+const compress = require('compression');
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const routes = require('./routes');
+
+const app = express();
+const buildPath = path.resolve(__dirname, 'public');
+
+app.use(compress());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('combined'));
+app.use(express.static(buildPath));
+app.use(routes.web);
+app.use('/api', routes.api);
+
+app.listen(3000);
