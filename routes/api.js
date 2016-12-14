cbf06b67effe50a3767abb6f5899f1abac2db49b
Jonathan Chrisp <jonathan.chrisp@gmail.com>
2016-12-14 19:23:30 +0000

    NOTICKET added airlines, airports and search api routes

---
+++ routes/api.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+const arrayHelper = require('../helpers/arrayHelper');
+const requestHelper = require('../helpers/requestHelper');
+
+const router = new express.Router();
+
+function onRequestSuccess(results) {
+    this.status(200).json(results);
+}
+
+function onRequestError(error) {
+    if (error && error.message) {
+        return this.status(400).send(error.message)
+    }
+
+    this.status(400).send();
+}
+
+router.get('/airlines', (req, res) => {
+    requestHelper.fetchAirlines()
+        .then(requestSuccess.bind(res))
+        .catch(requestError.bind(res));
+});
+
+router.get('/airports', (req, res) => {
+    const cityName = req.query.cityName;
+
+    requestHelper.fetchAirports(cityName)
+        .then(requestSuccess.bind(res))
+        .catch(requestError.bind(res));
+});
+
+router.post('/search', (req, res) => {
+    const { date, origin, destination } = req.body;
+    const sortKey = 'price';
+
+    return requestHelper.fetchAirportCodesAndAirlines(origin, destination)
+        .then(requestHelper.generateRoutes)
+        .then(requestHelper.fetchFlightsForRoutes.bind(null, date))
+        .then(arrayHelper.flattenArrays)
+        .then(arrayHelper.orderBy.bind(null, sortKey))
+        .then(onRequestSuccess.bind(res))
+        .catch(onRequestError.bind(res));
+});
+
+module.exports = router;
